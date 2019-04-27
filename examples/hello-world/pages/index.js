3c56f663e6b32863eb4e4c37fca0535191211ebf
maxpanyong <50037103+maxpanyong@users.noreply.github.com>
2019-04-27 10:49:00 +0800

    test

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default () => (
   <div>
-    Hello World.{' '}
-    <Link href='/about'>
+    Hello Wooooorld.{" "}
+    <Link href="/about">
       <a>About</a>
     </Link>
   </div>
-)
+);
